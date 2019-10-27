707ab51528dd4a42d4ef1dfe0907eaa6a415759f
kingboyk <kingboyk@kingboyk.com>
2019-10-27 18:15:52 +0000

    Blank WinForms project

---
+++ Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Thinkpad_Backlight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
